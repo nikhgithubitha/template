1a656a2233cf8b4a9c8c592392faf1b9d1b2fdd1
nikhgithubitha <nikhithakothamasu@gmail.con>
2023-11-23 09:18:13 +0000

    first commit

---
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -0,0 +1,63 @@
+const ActiveEventRegistration = props => {
+  const {details} = props
+
+  const renderClosed = () => (
+    return (
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+          alt="registrations closed"
+        />
+        <h1>Registrations are closed now!</h1>
+        <p>Stay tuned we will open the registrations soon</p>
+      </div>
+    )
+  )
+  
+
+  const renderToRegister = () => (
+    return (
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+          alt="yet to register"
+        />
+        <p>fill the form</p>
+        <button type="button">RegisterNow</button>
+      </div>
+    )
+  )
+
+  const renderRegistered = () => (
+    return (
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
+          alt="registered"
+        />
+        <p>You are already registered!</p>
+      </div>
+    )
+  ) 
+
+  const renderView = () => (
+    return (
+      <div>
+        <p>Click here to know the registration status</p>
+      </div>
+    )
+  )
+
+  switch (details) {
+    case 'REGISTRATIONS_CLOSED':
+      return renderClosed()
+    case 'YET_TO_REGISTER':
+      return renderToRegister()
+    case 'REGISTERED':
+      return renderRegistered()
+    default:
+      return renderView()
+  }
+}
+
+export default ActiveEventRegistration
